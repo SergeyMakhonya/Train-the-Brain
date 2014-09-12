6245ee418b733752a43fa0a2dd7c838047a5016d
Vlad-ku <tmnt-95@mail.ru>
2014-09-12 21:31:19 +0900

    первый полезный коммит

---
+++ _Build(32).bat
@@ -0,0 +1,2 @@
+"%PROGRAMFILES%\MSBuild\12.0\Bin\msbuild.exe" "Train the Brain.sln" /p:Configuration=Release;Platform=Win32
+pause
--- _Build.bat
+++
@@ -1,2 +0,0 @@
-"%PROGRAMFILES(x86)%\MSBuild\12.0\Bin\msbuild.exe" "Train the Brain.sln" /p:Configuration=Release;Platform=Win32
-pause
